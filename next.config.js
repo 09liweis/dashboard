da79832018c92ec2eb43a2c5bc1f702e5d2c0dba
Sam Li <weisen.li@hotmail.com>
2022-08-07 23:37:00 -0400

    add more image domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['img3.doubanio.com','gss2.bdstatic.com','wx4.sinaimg.cn','ia.media-imdb.com','m.media-amazon.com','img5.mtime.cn','img9.doubanio.com','upload.wikimedia.org','images-na.ssl-images-amazon.com'],
+    domains: ['gss3.bdstatic.com','img3.doubanio.com','gss2.bdstatic.com','wx4.sinaimg.cn','ia.media-imdb.com','m.media-amazon.com','img5.mtime.cn','img9.doubanio.com','upload.wikimedia.org','images-na.ssl-images-amazon.com'],
   },
 }
 
