4fbcbf2b4c34acacd9f5554016d2aaecbce19d57
Sam Li <weisen.li@hotmail.com>
2022-08-07 23:29:32 -0400

    add image domain support

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['img3.doubanio.com','gss2.bdstatic.com','wx4.sinaimg.cn','ia.media-imdb.com','m.media-amazon.com','img5.mtime.cn','img9.doubanio.com','upload.wikimedia.org','images-na.ssl-images-amazon.com'],
+  },
 }
 
 module.exports = nextConfig
