be1b524b7968cf04b9e0b64f97f9bbf19a4e7222
Sam Li <weisen.li@hotmail.com>
2022-08-08 23:53:58 -0400

    update config domains

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['www.hktvdrama.com','puui.qpic.cn','gss3.bdstatic.com','img3.doubanio.com','gss2.bdstatic.com','wx4.sinaimg.cn','ia.media-imdb.com','m.media-amazon.com','img5.mtime.cn','img9.doubanio.com','upload.wikimedia.org','images-na.ssl-images-amazon.com'],
+    domains: ['image.biccamera.com','ww4.sinaimg.cn','www.hktvdrama.com','puui.qpic.cn','gss3.bdstatic.com','img3.doubanio.com','gss2.bdstatic.com','wx4.sinaimg.cn','ia.media-imdb.com','m.media-amazon.com','img5.mtime.cn','img9.doubanio.com','upload.wikimedia.org','images-na.ssl-images-amazon.com'],
   },
 }
 
