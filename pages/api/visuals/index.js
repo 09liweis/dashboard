8cc9b28110ddaba1a5026fe7891d2f33fa1e8e65
Sam Li <weisen.li@hotmail.com>
2022-11-19 21:41:09 -0500

    feat(visual): add post visual api

--- pages/api/visuals/index.js
+++ pages/api/visuals/index.js
@@ -5,11 +5,25 @@ import connectDb from '../../../db/config'
 connectDb()
 
 export default async function handler(req,res) {
+  const method = req.method;
   try {
-    const query = {};
-    const opts = {limit:20};
-    const visuals = await VisualModel.find(query,'',opts).sort('-date_updated')
-    return res.status(200).json({ visuals})
+    if (method == 'GET') {
+      const query = {};
+      const opts = {limit:20};
+      const visuals = await VisualModel.find(query,'',opts).sort('-date_updated')
+      return res.status(200).json({ visuals})
+    } else if (method == 'POST') {
+      const update = req.body;
+      if (!update.douban_id) {
+        return res.status(200).json({msg:'No douban id'});
+      }
+      const filter = {douban_id:update.douban_id};
+      const doc = await Character.findOneAndUpdate(filter, update, {
+        returnOriginal: false
+      });
+
+      return res.status(200).json({visual:doc});
+    }
   } catch (err) {
     res.status(500).json({ error: 'failed to load data' })
   }
