eb13c89e8f9749a570b6ce08ca596d23051b5724
09liweis <weisen.li@hotmail.com>
2023-01-26 13:34:31 -0500

    update movie icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
-import type { GetServerSideProps, GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
+import type { GetServerSideProps, GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  useEffect(()=>{
-  },[]);
+  useEffect(() => {}, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -18,41 +17,41 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <section className='flex flex-wrap'>
+        <section className="flex flex-wrap">
           <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
             <div className="card p-2 shadow bg-blue-400">
-              <h2 className='text-2xl text-white'>Todos</h2>
+              <h2 className="text-2xl text-white">Todos</h2>
               <i className="fa-solid text-white text-xl fa-list"></i>
             </div>
           </article>
           <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
             <div className="card p-2 shadow bg-red-400">
-              <h2 className='text-2xl text-white'>Comments</h2>
+              <h2 className="text-2xl text-white">Comments</h2>
               <i className="fa-solid text-white text-xl fa-comments"></i>
             </div>
           </article>
           <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
             <div className="card p-2 shadow bg-green-400">
-              <h2 className='text-2xl text-white'>Expense</h2>
+              <h2 className="text-2xl text-white">Expense</h2>
               <i className="fa-solid text-white text-xl fa-dollar-sign"></i>
             </div>
           </article>
           <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
             <div className="card p-2 shadow bg-indigo-500">
-              <h2 className='text-2xl text-white'>Places</h2>
+              <h2 className="text-2xl text-white">Places</h2>
               <i className="fa-solid text-white text-xl fa-location-dot"></i>
             </div>
           </article>
           <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
             <div className="card p-2 shadow bg-purple-400">
-              <h2 className='text-2xl text-white'>Blog</h2>
+              <h2 className="text-2xl text-white">Blog</h2>
               <i className="fa-solid text-white text-xl fa-blog"></i>
             </div>
           </article>
           <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-            <div className="card p-2 shadow bg-purple-400">
-              <h2 className='text-2xl text-white'>Movie</h2>
-              <i className="fa-solid text-white text-xl fa-camera-movie"></i>
+            <div className="card p-2 shadow bg-purple-600">
+              <h2 className="text-2xl text-white">Movie</h2>
+              <i className="fa-solid text-white text-xl fa-film"></i>
             </div>
           </article>
         </section>
@@ -71,15 +70,15 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export const getStaticProps:GetServerSideProps = async (ctx) => {
+export const getStaticProps: GetServerSideProps = async (ctx) => {
   return {
-    props:{
-      visual:{}
-    }
-  }
-}
+    props: {
+      visual: {},
+    },
+  };
+};
 
-export default Home
+export default Home;
