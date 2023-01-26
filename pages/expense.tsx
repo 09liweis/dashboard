e6c872ead15a7776798cd0eba50f02ff88e3d039
09liweis <weisen.li@hotmail.com>
2023-01-26 14:21:08 -0500

    style(home): remove unuse css, style nav in home page

--- pages/expense.tsx
+++ pages/expense.tsx
@@ -1,37 +1,40 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import React, { useEffect, useState, useCallback } from 'react'
-import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
-import styles from '../styles/Home.module.css'
-import { fetchAPI } from '../helpers'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import React, { useEffect, useState, useCallback } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import styles from '../styles/Home.module.css';
+import { fetchAPI } from '../helpers';
 
-const EXPANSE_API_DATA:string = 'https://samliweisen.onrender.com/api/transactions/statistics';
+const EXPANSE_API_DATA: string =
+  'https://samliweisen.onrender.com/api/transactions/statistics';
 
 interface SingleExpense {
-  name:string,
-  y:number
+  name: string;
+  y: number;
 }
 
 interface CategoryTransaction {
-  total: number,
-  items: [{
-    _id:string,
-    price:number,
-    date: string,
-    title:string,
-    place:{
-      _id:string,
-      name:string
+  total: number;
+  items: [
+    {
+      _id: string;
+      price: number;
+      date: string;
+      title: string;
+      place: {
+        _id: string;
+        name: string;
+      };
     }
-  }]
+  ];
 }
 
 interface CategoryTransactions {
-  [key:string]: CategoryTransaction
+  [key: string]: CategoryTransaction;
 }
 
-const ICON_CATEGORY:{[key:string]:string} = {
+const ICON_CATEGORY: { [key: string]: string } = {
   food: 'fa-utensils',
   gas: 'fa-gas-pump',
   computer: 'fa-computer',
@@ -39,58 +42,62 @@ const ICON_CATEGORY:{[key:string]:string} = {
   grocery: 'fa-cart-shopping',
   phone: 'fa-phone',
   electricity: 'fa-bolt',
-  internet: 'fa-internet-explorer'
+  internet: 'fa-internet-explorer',
 };
 
-const YEARS = ['2023','2022','2021','2020'];
-const MONTHS:{[key:string]:string} = {
-  '1':'Jan',
-  '2':'Fed',
-  '3':'Mar',
-  '4':'Apr',
-  '5':'May',
-  '6':'June',
-  '7':'July',
-  '8':'Aug',
-  '9':'Sep',
-  '10':'Oct',
-  '11':'Nov',
-  '12':'Dec'
-}
+const YEARS = ['2023', '2022', '2021', '2020'];
+const MONTHS: { [key: string]: string } = {
+  '1': 'Jan',
+  '2': 'Fed',
+  '3': 'Mar',
+  '4': 'Apr',
+  '5': 'May',
+  '6': 'June',
+  '7': 'July',
+  '8': 'Aug',
+  '9': 'Sep',
+  '10': 'Oct',
+  '11': 'Nov',
+  '12': 'Dec',
+};
 
 const Expense: NextPage = () => {
-  const emptyExpenses:Array<SingleExpense> = [];
-  const emptyTransactions:CategoryTransactions = {};
+  const emptyExpenses: Array<SingleExpense> = [];
+  const emptyTransactions: CategoryTransactions = {};
   const [curYear, setYear] = useState('');
   const [curMonth, setMonth] = useState('');
   const [curTotal, setTotal] = useState(0);
-  const [categoryTransactions, setCategoryTransactions] = useState(emptyTransactions);
-  const [expenses,setExpenses] = useState(emptyExpenses);
-  const [expenseNm, setExpenseNm] = useState('')
-  const [expenseVal, setExpenseVal] = useState(0)
+  const [categoryTransactions, setCategoryTransactions] =
+    useState(emptyTransactions);
+  const [expenses, setExpenses] = useState(emptyExpenses);
+  const [expenseNm, setExpenseNm] = useState('');
+  const [expenseVal, setExpenseVal] = useState(0);
   const getExpenseStatistics = useCallback(async () => {
-    const body = {date:''};
+    const body = { date: '' };
     if (curYear || curMonth) {
       body.date = `${curYear}`;
       if (curMonth) {
-        body.date += `-${parseInt(curMonth)<10?'0'+curMonth:curMonth}`
+        body.date += `-${parseInt(curMonth) < 10 ? '0' + curMonth : curMonth}`;
       }
     }
-    const {categoryPrice,total} = await fetchAPI({url:EXPANSE_API_DATA,body});
+    const { categoryPrice, total } = await fetchAPI({
+      url: EXPANSE_API_DATA,
+      body,
+    });
     setCategoryTransactions(categoryPrice);
     setTotal(total);
-    let expenses:Array<SingleExpense> = [];
+    let expenses: Array<SingleExpense> = [];
     for (const name in categoryPrice) {
       const y = Math.abs(categoryPrice[name].total);
-      expenses.push({name,y});
+      expenses.push({ name, y });
     }
     setExpenses(expenses);
-  },[curYear,curMonth])
-  useEffect(()=>{
+  }, [curYear, curMonth]);
+  useEffect(() => {
     getExpenseStatistics();
-  },[curYear, curMonth, getExpenseStatistics]);
+  }, [curYear, curMonth, getExpenseStatistics]);
 
-  const editInput = (e:any,field:string) =>{
+  const editInput = (e: any, field: string) => {
     const value = e.target.value;
     if (field == 'nm') {
       setExpenseNm(value);
@@ -98,8 +105,8 @@ const Expense: NextPage = () => {
     if (field == 'val') {
       setExpenseVal(parseFloat(value));
     }
-  }
-  const addExpense = () =>{
+  };
+  const addExpense = () => {
     if (expenseNm.trim() === '') {
       alert('Category can not be empty');
       return;
@@ -108,45 +115,47 @@ const Expense: NextPage = () => {
       alert('Expense can not be 0');
       return;
     }
-    expenses.push({name:expenseNm,y:expenseVal});
+    expenses.push({ name: expenseNm, y: expenseVal });
     setExpenses(expenses);
     setExpenseNm('');
     setExpenseVal(0);
-  }
+  };
 
-  const deleteExpense = (idx:number) => {
-    setExpenses(expenses.filter((item,i) => i !== idx));
-  }
+  const deleteExpense = (idx: number) => {
+    setExpenses(expenses.filter((item, i) => i !== idx));
+  };
 
   const options = {
     chart: {
       type: 'bar',
-      animation:{
-        duration:50
-      }
+      animation: {
+        duration: 50,
+      },
     },
     xAxis: {
-      type:'category'
+      type: 'category',
     },
     yAxis: {
       min: 0,
       title: {
         text: 'Expense amount',
-        align: 'high'
+        align: 'high',
       },
       labels: {
-        overflow: 'justify'
-      }
+        overflow: 'justify',
+      },
     },
     title: {
-      text: 'My Expense Chart'
+      text: 'My Expense Chart',
     },
-    series: [{
-      name: 'Expense',
-      colorByPoint:true,
-      data: expenses
-    }]
-  }
+    series: [
+      {
+        name: 'Expense',
+        colorByPoint: true,
+        data: expenses,
+      },
+    ],
+  };
 
   return (
     <div className={styles.container}>
@@ -156,67 +165,115 @@ const Expense: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h2>费用支出</h2>
-      <section className='flex mb-3'>
+      <section className="flex mb-3">
         <article className="month bg-white p-2 rounded">
-          {YEARS.map((year)=><p className={curYear==year?'text-red-300':''} key={year} onClick={()=>setYear(year)}>{year}</p>)}
-          {curYear?
-          Object.keys(MONTHS).map((month)=><p className={curMonth==month?'text-red-300':''} key={month} onClick={()=>setMonth(month)}>{MONTHS[month]}</p>)
-          :null}
+          {YEARS.map((year) => (
+            <p
+              className={curYear == year ? 'text-red-300' : ''}
+              key={year}
+              onClick={() => setYear(year)}
+            >
+              {year}
+            </p>
+          ))}
+          {curYear
+            ? Object.keys(MONTHS).map((month) => (
+                <p
+                  className={curMonth == month ? 'text-red-300' : ''}
+                  key={month}
+                  onClick={() => setMonth(month)}
+                >
+                  {MONTHS[month]}
+                </p>
+              ))
+            : null}
         </article>
 
         <article className="pay rounded ml-5">
-          <p className="total"><i className="fa-solid fa-sack-dollar"></i>Total:{curTotal}</p>
-          {Object.keys(categoryTransactions).map((key)=>{
-            const {total, items} = categoryTransactions[key];
+          <p className="total">
+            <i className="fa-solid fa-sack-dollar"></i>Total:{curTotal}
+          </p>
+          {Object.keys(categoryTransactions).map((key) => {
+            const { total, items } = categoryTransactions[key];
             return (
               <div key={key}>
                 <div className="father">
-                  <span className='type'><i className={`fa-solid ${ICON_CATEGORY[key]}`}></i>{key}</span>
-                  <span className='money'><i className="fa-solid fa-sack-dollar"></i>{total}</span>
-                </div>  
+                  <span className="type">
+                    <i className={`fa-solid ${ICON_CATEGORY[key]}`}></i>
+                    {key}
+                  </span>
+                  <span className="money">
+                    <i className="fa-solid fa-sack-dollar"></i>
+                    {total}
+                  </span>
+                </div>
                 <ul className="detail1">
-                  {items.map(({_id,price,date,place,title})=>{
+                  {items.map(({ _id, price, date, place, title }) => {
                     return (
-                      <li key={_id}>{date} {place.name} {title} 支出${price}</li>
-                    )
+                      <li key={_id}>
+                        {date} {place.name} {title} 支出${price}
+                      </li>
+                    );
                   })}
                 </ul>
               </div>
-            )
+            );
           })}
         </article>
       </section>
-    
+
       <main>
-        {expenses.length?
-        <React.Fragment>
-          <HighchartsReact
-            highcharts={Highcharts}
-            options={options}
-          />
-          <h3>Total: {expenses.reduce((pVal,cVal)=> pVal + cVal.y,0).toFixed(2)}</h3>
-          <section className='margin-tb-10'>
-            {
-              expenses.map((expense,idx)=>{
+        {expenses.length ? (
+          <React.Fragment>
+            <HighchartsReact highcharts={Highcharts} options={options} />
+            <h3>
+              Total:{' '}
+              {expenses.reduce((pVal, cVal) => pVal + cVal.y, 0).toFixed(2)}
+            </h3>
+            <section className="margin-tb-10">
+              {expenses.map((expense, idx) => {
                 return (
-                  <article className='border border-radius padding-5 display-flex justify-content-sb margin-b-10' key={expense.name}>
-                    <span>{expense.name}: {expense.y.toFixed(2)}</span>
-                    <span onClick={()=>deleteExpense(idx)}>Delete</span>
+                  <article className="border" key={expense.name}>
+                    <span>
+                      {expense.name}: {expense.y.toFixed(2)}
+                    </span>
+                    <span onClick={() => deleteExpense(idx)}>Delete</span>
                   </article>
-                )
-              })
-            }
+                );
+              })}
+            </section>
+          </React.Fragment>
+        ) : (
+          <section className="center">
+            Please add some expenses to show the chart
           </section>
-        </React.Fragment>
-        :<section className='center'>Please add some expenses to show the chart</section>}
-        <div className='display-flex'>
-          <input className='width-100 margin-b-10 padding-10' id='nm' placeholder='Enter Category' value={expenseNm} onChange={(e)=>editInput(e,'nm')} />
-          <input className='width-100 margin-b-10 padding-10' id='val' type='number' value={expenseVal} placeholder='Enter Expense Amount' onChange={(e)=>editInput(e,'val')}/>
-          <button className='width-40 margin-b-10 padding-10' onClick={addExpense}>Add</button>
+        )}
+        <div className="display-flex">
+          <input
+            className="width-100 margin-b-10 padding-10"
+            id="nm"
+            placeholder="Enter Category"
+            value={expenseNm}
+            onChange={(e) => editInput(e, 'nm')}
+          />
+          <input
+            className="width-100 margin-b-10 padding-10"
+            id="val"
+            type="number"
+            value={expenseVal}
+            placeholder="Enter Expense Amount"
+            onChange={(e) => editInput(e, 'val')}
+          />
+          <button
+            className="width-40 margin-b-10 padding-10"
+            onClick={addExpense}
+          >
+            Add
+          </button>
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Expense
+export default Expense;
