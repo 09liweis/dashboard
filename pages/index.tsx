9d22e01981169e1647981f3323d9ecca16f4615d
Sam Li <weisen.li@hotmail.com>
2023-01-26 11:01:47 -0500

    style(home): add responsive class to card

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,25 +19,25 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <section className='flex flex-wrap'>
-          <article className="p-2 w-1/4">
+          <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
             <div className="card p-2 shadow bg-blue-400">
               <h2 className='text-2xl text-white'>Todos</h2>
               <i className="fa-solid text-white text-xl fa-list"></i>
             </div>
           </article>
-          <article className="p-2 w-1/4">
+          <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
             <div className="card p-2 shadow bg-red-400">
               <h2 className='text-2xl text-white'>Comments</h2>
               <i className="fa-solid text-white text-xl fa-comments"></i>
             </div>
           </article>
-          <article className="p-2 w-1/4">
+          <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
             <div className="card p-2 shadow bg-green-400">
               <h2 className='text-2xl text-white'>Expense</h2>
               <i className="fa-solid text-white text-xl fa-dollar-sign"></i>
             </div>
           </article>
-          <article className="p-2 w-1/4">
+          <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
             <div className="card p-2 shadow bg-indigo-500">
               <h2 className='text-2xl text-white'>Places</h2>
               <i className="fa-solid text-white text-xl fa-location-dot"></i>
