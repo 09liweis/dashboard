5b32644cc984f1e65429deb7028bea3ea4a2c2a9
09liweis <weisen.li@hotmail.com>
2023-04-05 13:38:55 -0400

    feat(todo): fetch todos on dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,27 +4,68 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import Icon from '../components/Icon';
+import { TODO_LIST_API } from '../constants';
+import { fetchAPI } from '../helpers';
 import styles from '../styles/Home.module.css';
 
 const DASHBOARD_CARDS = [
-  { tl: 'Todos', icon: 'list', bg: 'blue-400' },
-  { tl: 'Comments', icon: 'comments', bg: 'red-400' },
-  { tl: 'Expense', icon: 'dollar-sign', bg: 'green-400' },
-  { tl: 'Places', icon: 'location-dot', bg: 'indigo-500' },
-  { tl: 'Blogs', icon: 'blog', bg: 'purple-500' },
-  { tl: 'Movie', icon: 'film', bg: 'purple-600' },
+  { tl: 'todos', icon: 'list', bg: 'blue-400' },
+  { tl: 'comments', icon: 'comments', bg: 'red-400' },
+  { tl: 'expense', icon: 'dollar-sign', bg: 'green-400' },
+  { tl: 'places', icon: 'location-dot', bg: 'indigo-500' },
+  { tl: 'blogs', icon: 'blog', bg: 'purple-500' },
+  { tl: 'movie', icon: 'film', bg: 'purple-600' },
 ];
 
+interface Todo {
+  _id: string;
+  name: string;
+  status: string;
+  date: string;
+}
+
+interface Todos {
+  [index: number]: Todo;
+}
+
 const Home: NextPage = () => {
-  useEffect(() => {}, []);
+  const emptyTodos: Todos = [];
+  const [todos, setTodos] = useState(emptyTodos);
+
+  async function fetchTodos() {
+    const response = await fetchAPI({
+      url: TODO_LIST_API,
+      method: 'GET',
+      body: {},
+    });
+    if (response) {
+      setTodos(response);
+    }
+  }
+
+  const renderCards = (type: string) => {
+    if (type == 'todos') {
+      const todosHTML = todos.map((todo) => (
+        <article key={todo._id}>{todo.name}</article>
+      ));
+      return todosHTML;
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
   return (
     <main className={styles.main}>
       <section className="flex flex-wrap">
         {DASHBOARD_CARDS.map(({ tl, icon, bg }) => (
-          <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+          <article key={tl} className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
             <div className={`card p-2 shadow bg-${bg}`}>
-              <h2 className="text-2xl text-white">{tl}</h2>
-              <Icon name={icon} classNames={'text-white text-xl'} />
+              <h2 className="text-2xl text-white">
+                <Icon name={icon} classNames={'mr-2'} />
+                {tl}
+              </h2>
+              {renderCards(tl)}
             </div>
           </article>
         ))}
