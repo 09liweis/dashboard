6e1e682eb89aaeb5913aa5dbcde2a9b97a561ff9
09liweis <weisen.li@hotmail.com>
2023-04-05 11:27:00 -0400

    feat(constant): add some api constants

--- constants.tsx
+++ constants.tsx
@@ -1,9 +1,12 @@
 export const buttonStyle =
   'p-1 text-center rounded cursor-pointer text-white bg-red-400 transition duration-300 hover:rotate-12 hover:bg-red-600';
 
+export const API_SERVER = 'https://samliweisen.onrender.com/api/';
 
-export const API_SERVER = 'https://samliweisen.onrender.com/';
+export const EXPENSE_LIST_API = `${API_SERVER}transactions/statistics`;
 
-export const EXPENSE_LIST_API = `${API_SERVER}api/transactions/statistics`;
+export const EXPENSE_CATEGORIES_API = `${API_SERVER}transactions/categories`;
 
-export const EXPENSE_CATEGORIES_API = `${API_SERVER}api/transactions/categories`;
+export const TODO_LIST_API = `${API_SERVER}todos`;
+
+export const BLOG_LIST_API = `${API_SERVER}blogs`;
