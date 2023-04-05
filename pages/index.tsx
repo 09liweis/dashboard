e0b3993518ecd9e1d239acfe67cc3cbfc29a5a6d
09liweis <weisen.li@hotmail.com>
2023-04-05 13:03:22 -0400

    refactor(home): add card data

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,49 +3,31 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import Icon from '../components/Icon';
 import styles from '../styles/Home.module.css';
+
+const DASHBOARD_CARDS = [
+  { tl: 'Todos', icon: 'list', bg: 'blue-400' },
+  { tl: 'Comments', icon: 'comments', bg: 'red-400' },
+  { tl: 'Expense', icon: 'dollar-sign', bg: 'green-400' },
+  { tl: 'Places', icon: 'location-dot', bg: 'indigo-500' },
+  { tl: 'Blogs', icon: 'blog', bg: 'purple-500' },
+  { tl: 'Movie', icon: 'film', bg: 'purple-600' },
+];
 
 const Home: NextPage = () => {
   useEffect(() => {}, []);
   return (
     <main className={styles.main}>
       <section className="flex flex-wrap">
-        <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-          <div className="card p-2 shadow bg-blue-400">
-            <h2 className="text-2xl text-white">Todos</h2>
-            <i className="fa-solid text-white text-xl fa-list"></i>
-          </div>
-        </article>
-        <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-          <div className="card p-2 shadow bg-red-400">
-            <h2 className="text-2xl text-white">Comments</h2>
-            <i className="fa-solid text-white text-xl fa-comments"></i>
-          </div>
-        </article>
-        <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-          <div className="card p-2 shadow bg-green-400">
-            <h2 className="text-2xl text-white">Expense</h2>
-            <i className="fa-solid text-white text-xl fa-dollar-sign"></i>
-          </div>
-        </article>
-        <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-          <div className="card p-2 shadow bg-indigo-500">
-            <h2 className="text-2xl text-white">Places</h2>
-            <i className="fa-solid text-white text-xl fa-location-dot"></i>
-          </div>
-        </article>
-        <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-          <div className="card p-2 shadow bg-purple-400">
-            <h2 className="text-2xl text-white">Blog</h2>
-            <i className="fa-solid text-white text-xl fa-blog"></i>
-          </div>
-        </article>
-        <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-          <div className="card p-2 shadow bg-purple-600">
-            <h2 className="text-2xl text-white">Movie</h2>
-            <i className="fa-solid text-white text-xl fa-film"></i>
-          </div>
-        </article>
+        {DASHBOARD_CARDS.map(({ tl, icon, bg }) => (
+          <article className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+            <div className={`card p-2 shadow bg-${bg}`}>
+              <h2 className="text-2xl text-white">{tl}</h2>
+              <Icon name={icon} classNames={'text-white text-xl'} />
+            </div>
+          </article>
+        ))}
       </section>
     </main>
   );
