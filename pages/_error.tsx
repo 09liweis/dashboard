04500e3489273ef122d362862f0014feff0ffbde
09liweis <weisen.li@hotmail.com>
2023-05-04 11:54:24 -0400

    fix(404): image width/height

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -5,12 +5,7 @@ export default function ErrorPage({ Component }: AppProps) {
   return (
     <section className="text-center">
       <h1 className="text-xl font-bold font-mono">Are you lost?</h1>
-      <Image
-        src="/images/404.svg"
-        width={'400px'}
-        height={'400px'}
-        alt={'404'}
-      />
+      <Image src="/images/404.svg" width={400} height={400} alt={'404'} />
     </section>
   );
 }
