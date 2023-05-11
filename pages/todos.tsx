d5676adf38cc5533f0fa2fc94f554e6701157e06
09liweis <weisen.li@hotmail.com>
2023-05-11 15:19:03 -0400

    feat(todo): render todo list

--- pages/todos.tsx
+++ pages/todos.tsx
@@ -1,14 +1,46 @@
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import AppContext from '../AppContext';
+import { API_SERVER, TODO_LIST_API } from '../constants';
+import { fetchAPI } from '../helpers';
+
+interface Todo {
+  _id: string;
+  name: string;
+  status: string;
+  date: string;
+}
 
 const TodosPage: NextPage = () => {
-  useEffect(() => {}, []);
+  const emptyTodos: Array<Todo> = [];
+  const [todos, setTodos] = useState(emptyTodos);
 
-  return (
-    <section>
-      <h1>Todos</h1>
-    </section>
-  );
+  const { user } = useContext(AppContext);
+
+  const fetchTodos = async () => {
+    const response = await fetchAPI({
+      method: 'GET',
+      url: TODO_LIST_API,
+      body: {},
+    });
+    setTodos(response);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const todosHTML = todos.map((todo) => (
+    <article
+      key={todo._id}
+      className="shadow-lg bg-white/[30%] rounded-lg p-2 mb-2 flex justify-between"
+    >
+      <h2 className="text-teal-600">{todo.name}</h2>
+      <span className="text-blue-600">{todo.date}</span>
+    </article>
+  ));
+
+  return <section>{todosHTML}</section>;
 };
 
 export default TodosPage;
