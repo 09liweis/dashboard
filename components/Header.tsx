c0ec0f16b207a54e36910473c382893c643bb79d
09liweis <weisen.li@hotmail.com>
2023-06-14 09:45:04 -0400

    hide videos page

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const NAV_LINKS = [
   { tl: 'home', url: '/', icon: 'house-user' },
   { tl: 'knowledges', url: '/knowledges', icon: 'book' },
   { tl: 'todos', url: '/todos', icon: 'list' },
-  { tl: 'videos', url: '/videos', icon: 'video' },
+  // { tl: 'videos', url: '/videos', icon: 'video' },
   { tl: 'expense', url: '/expense', icon: 'piggy-bank' },
   { tl: 'blogs', url: '/blogs', icon: 'piggy-bank' },
   { tl: 'subscription', url: '/newsletter', icon: 'envelope' },
