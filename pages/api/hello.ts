6062fde32aa09a4e8377e70b681b9764564e8aa7
09liweis <weisen.li@hotmail.com>
2023-06-15 11:09:42 -0400

    test os module

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import os from 'os';
 
 type Data = {
   name: string;
@@ -9,5 +10,19 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log(os.cpus()[0]);
+  let currentOS = {
+    name: os.type(),
+    architecture: os.arch(),
+    platform: os.platform(),
+    release: os.release(),
+    version: os.version(),
+  };
+
+  console.log(currentOS);
+  console.log(`The server has been up for ${os.uptime()} seconds.`);
+  console.log(os.userInfo());
+  console.log(os.networkInterfaces());
+
   res.status(200).json({ name: 'John Doe' });
 }
