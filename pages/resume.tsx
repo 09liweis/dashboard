51d9b770384a6dd182dc6c7d951c615a725eabce
Replit user <>
2023-08-09 16:01:29 +0000

    initial resume page

---
+++ pages/resume.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const ResumePage: NextPage = () => {
+  return (
+    <>
+      <h1 className='text-center font-bold text-lg'>My Resume</h1>
+      <section className='mt-4'></section>
+    </>
+  );
+}
+
+export default ResumePage;
