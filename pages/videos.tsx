1345cfafb8df3e0dfe3a1e68bb4e11731909a828
Replit user <>
2023-10-13 14:52:41 +0000

    update videos update

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -18,12 +18,12 @@ const VideosPage: NextPage = () => {
   const [videos, setVideos] = useState(emptyVideos);
 
   const fetchVideos = async () => {
-    const response = await fetchAPI({
-      method: 'POST',
+    const { allVideos } = await fetchAPI({
+      method: 'GET',
       url: VIDEO_LIST_API,
       body: {},
     });
-    setVideos(response);
+    setVideos(allVideos || []);
   };
 
   useEffect(() => {
@@ -33,24 +33,29 @@ const VideosPage: NextPage = () => {
   const videosHTML = videos.map((v) => (
     <article
       key={v.bvid}
-      className="relative shadow-lg bg-white/[30%] rounded-lg p-2 mb-2 flex justify-between"
+      className="relative shadow-lg bg-white/[30%] rounded-lg p-2 mb-2 flex justify-between sm:w-1/2 md:w-1/3 lg:w-1/4"
     >
-      {v.pic && (
-        <Image
-          src={v.pic}
-          className="w-full"
-          alt={v.title}
-          objectFit="cover"
-          layout="fill"
-        />
-      )}
+      {v.pic &&
+        (
+          <Image
+            src={v.pic}
+            className="w-full h-100 rounded h-56"
+            alt={v.title}
+            objectFit="cover"
+            layout="fill"
+          />
+        )
+        /*< img src={v.pic} className="w-full rounded-lg" alt={v.title} />*/
+      }
     </article>
   ));
 
   return (
     <section>
       <h1 className="text-center font-bold text-2xl mb-2">In Construction</h1>
-      {videosHTML}
+      <section className='flex flex-wrap'>
+        {videosHTML}
+      </section>
     </section>
   );
 };
