599d6539233965d3251c18f3b1815c2468b111f6
Sam Li <weisen.li@hotmail.com>
2024-03-03 19:13:32 +0000

    handle error on movie api

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -2,7 +2,11 @@ import Movie from "../../Models/Movie";
 import connectDB from "../../lib/connectDB"
 
 export default async function handler(req, res) {
-  await connectDB()
-  const movies = await Movie.find().limit(50).sort("-date_updated");
-  res.status(200).json({ movies })
+  try {
+    await connectDB()
+    const movies = await Movie.find().limit(50).sort("-date_updated");
+    res.status(200).json({ movies })
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
 }
