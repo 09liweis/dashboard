a29b2fcd8a6451e196cb544f3613af5da1275496
Sam Li <weisen.li@hotmail.com>
2024-03-04 16:35:53 +0000

    add random color to icon

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -3,6 +3,11 @@ interface IconProps {
   classNames?: string;
   text?: string;
 }
+
+function getRandomColor() {
+  return `#${Math.floor(Math.random()*0xffffff).toString(16)}`
+}
+
 export default function Icon({ name, classNames = '', text = '' }: IconProps) {
-  return <i className={`fa-solid fa-${name} ${classNames}`}>{text}</i>;
+  return <i style={{color:getRandomColor()}} className={`fa-solid fa-${name} ${classNames}`}>{text}</i>;
 }
