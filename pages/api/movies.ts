eca0d0f3213d67ffde7da7dcc095e0b23da9a8c4
Sam Li <weisen.li@hotmail.com>
2024-06-24 15:02:08 +0000

    update movies to typescript

--- pages/api/movies.ts
+++ pages/api/movies.ts
@@ -1,12 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import Movie from "../../Models/Movie";
 import connectDB from "../../lib/connectDB"
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     await connectDB()
     const movies = await Movie.find().limit(50).sort("-date_updated");
     res.status(200).json({ movies })
-  } catch (error) {
+  } catch (error: any) {
     res.status(500).json({ error: error.toString() });
   }
 }
