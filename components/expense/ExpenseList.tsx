d1fcb6965765d00505b656c9b23de91f700d4d57
Sam Li <weisen.li@hotmail.com>
2024-09-14 22:40:30 +0000

    refactor expense list

--- components/expense/ExpenseList.tsx
+++ components/expense/ExpenseList.tsx
@@ -1,4 +1,33 @@
-import { ExpenseListProps } from 'types';
+import { ExpenseListProps, Transaction } from 'types';
+
+interface ExpensesProps {
+  expenses: Transaction[];
+  openTransactionDetail: Function;
+}
+const Expenses = ({ expenses, openTransactionDetail }: ExpensesProps) => {
+  return (
+    <>
+      {expenses.map((transaction) => {
+        const { id, price, date, place, title } = transaction;
+        return (
+          <article
+            key={id}
+            title={place?.address}
+            className="flex px-1 justify-between cursor-pointer text-red-400 hover:text-red-600 transition duration-300"
+            onClick={() => openTransactionDetail(transaction)}
+          >
+            <span className="truncate w-1/2">{place?.name}</span>
+            <span className="flex justify-between w-1/2 text-black">
+              <span className="text-indigo-500">{date}</span>
+              <span className="truncate hidden md:block">{title}</span>
+              {price && <span className='text-rose-600'>{price}</span>}
+            </span>
+          </article>
+        );
+      })}
+    </>
+  )
+}
 
 export default function ExpenseList({
   categoryTransactions,
@@ -12,26 +41,9 @@ export default function ExpenseList({
             <span className='capitalize text-xl'>{category}</span>
             <span className="font-bold">{total}</span>
           </div>
-          <div className='bg-red-500 text-sm text-white text-right p-1 rounded' style={{width:percentage+'%'}}>{percentage.toFixed(2)}%</div>
+          <div className='bg-red-500 text-sm text-white text-right p-1 rounded' style={{ width: percentage + '%' }}>{percentage.toFixed(2)}%</div>
         </div>
-        {items.map((transaction) => {
-          const { id, price, date, place, title } = transaction;
-          return (
-            <article
-              key={id}
-              title={place?.address}
-              className="flex px-1 justify-between cursor-pointer text-red-400 hover:text-red-600 transition duration-300"
-              onClick={() => openTransactionDetail(transaction)}
-            >
-              <span className="truncate w-1/2">{place?.name}</span>
-              <span className="flex justify-between w-1/2 text-black">
-                <span className="text-indigo-500">{date}</span>
-                <span className="truncate hidden md:block">{title}</span>
-                {price && <span className='text-rose-600'>{price}</span>}
-              </span>
-            </article>
-          );
-        })}
+        <Expenses expenses={items} openTransactionDetail={openTransactionDetail} />
       </div>
     );
   });
