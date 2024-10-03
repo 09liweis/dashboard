8e0860d0456a659d4c2a7ef58e0b8f0b39c12fd3
09liweis <weisen.li@hotmail.com>
2024-10-03 15:31:48 -0400

    fix tailwind by add css classes

--- components/expense/ExpenseList.tsx
+++ components/expense/ExpenseList.tsx
@@ -37,12 +37,12 @@ export default function ExpenseList({
     {categoryTransactions.map(({ category, total, items, percentage, income }) => {
       return (
         <div key={category} className="mb-2">
-          <div className={`category-price text-${income?'green':'red'}-600`}>
+          <div className={`category-price ${income?'income':'expense'}`}>
             <div className='flex justify-between items-center'>
               <span className='capitalize text-xl'>{category}</span>
               <span className="font-bold">{total}</span>
             </div>
-            <div className={`bg-${income ? 'green':'red'}-500 text-sm text-white text-right p-1 rounded`} style={{ width: percentage + '%' }}>{percentage.toFixed(2)}%</div>
+            <div className={`category-percentage ${income?'income':'expense'}`} style={{ width: percentage + '%' }}>{percentage.toFixed(2)}%</div>
           </div>
           <Expenses expenses={items} openTransactionDetail={openTransactionDetail} />
         </div>
