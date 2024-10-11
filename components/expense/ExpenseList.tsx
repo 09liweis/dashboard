05642091b502828031fa851ab2daf64cb9741cf7
09liweis <weisen.li@hotmail.com>
2024-10-11 09:29:24 -0400

    refactor categoryPrice component

--- components/expense/ExpenseList.tsx
+++ components/expense/ExpenseList.tsx
@@ -1,4 +1,4 @@
-import { ExpenseListProps, Transaction } from 'types';
+import { CategoryTransaction, ExpenseListProps, Transaction } from 'types';
 
 interface ExpensesProps {
   expenses: Transaction[];
@@ -20,7 +20,7 @@ const Expenses = ({ expenses, openTransactionDetail }: ExpensesProps) => {
             <span className="flex justify-between w-1/2 text-black">
               <span className="text-indigo-400">{date}</span>
               <span className="truncate hidden md:block">{title}</span>
-              {price && <span className={income?'text-green-600':`text-rose-600`}>{price}</span>}
+              <span className={income?'text-green-600':`text-rose-600`}>{price}</span>
             </span>
           </article>
         );
@@ -29,21 +29,29 @@ const Expenses = ({ expenses, openTransactionDetail }: ExpensesProps) => {
   )
 }
 
+const CategoryPrice = ({categoryPrice}:{categoryPrice:CategoryTransaction}) => {
+  const {category,income,total,percentage} = categoryPrice;
+  return (
+    <div className={`category-price ${income?'income':'expense'}`}>
+      <div className='flex justify-between items-center'>
+        <span className='capitalize text-xl'>{category}</span>
+        <span className="font-bold">{total}</span>
+      </div>
+      <div className={`category-percentage ${income?'income':'expense'}`} style={{ width: percentage + '%' }}>{percentage.toFixed(2)}%</div>
+    </div>
+  )
+}
+
 export default function ExpenseList({
   categoryTransactions,
   openTransactionDetail,
 }: ExpenseListProps) {
   return (<>
-    {categoryTransactions.map(({ category, total, items, percentage, income }) => {
+    {categoryTransactions.map((categoryPrice) => {
+      const {items, category} = categoryPrice;
       return (
         <div key={category} className="mb-2">
-          <div className={`category-price ${income?'income':'expense'}`}>
-            <div className='flex justify-between items-center'>
-              <span className='capitalize text-xl'>{category}</span>
-              <span className="font-bold">{total}</span>
-            </div>
-            <div className={`category-percentage ${income?'income':'expense'}`} style={{ width: percentage + '%' }}>{percentage.toFixed(2)}%</div>
-          </div>
+          <CategoryPrice categoryPrice={categoryPrice} />
           <Expenses expenses={items} openTransactionDetail={openTransactionDetail} />
         </div>
       );
