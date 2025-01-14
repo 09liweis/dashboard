821d8bfe83d71daf6407c8dfd912594696658583
09liweis <weisen.li@hotmail.com>
2025-01-14 16:53:40 -0500

    fix expense item margin

--- components/expense/ExpenseList.tsx
+++ components/expense/ExpenseList.tsx
@@ -11,7 +11,7 @@ const CategoryPrice = ({ categoryPrice }: CategoryPriceProps) => {
   const colorClass = income ? 'bg-green-600' : 'bg-red-600';
 
   return (
-    <div className="bg-white rounded-lg p-4 mb-4 border border-gray-100 shadow-sm">
+    <div className="bg-white rounded-lg p-4 mb-1 border border-gray-100 shadow-sm">
       <div className="flex justify-between items-center mb-2">
         <h3 className="text-lg font-medium capitalize">{category}</h3>
         <span className="font-bold text-lg">{total}</span>
@@ -40,7 +40,7 @@ export default function ExpenseList({
           exit={{ opacity: 0, y: -20 }}
           key={categoryPrice.category}>
           <CategoryPrice categoryPrice={categoryPrice} />
-          <div className="space-y-2">
+          <div className="space-y-1">
             {categoryPrice.items.map((transaction) => (
               <ExpenseTransaction
                 key={transaction._id}
