c5614f63329906d5ef7c88f06e77521503ba60f5
09liweis <weisen.li@hotmail.com>
2025-03-31 13:18:02 -0400

    style expense list

--- components/expense/ExpenseList.tsx
+++ components/expense/ExpenseList.tsx
@@ -12,10 +12,10 @@ const CategoryPrice = ({ categoryPrice }: CategoryPriceProps) => {
   const priceColor = income ? 'text-green-600' : 'text-red-600';
 
   return (
-    <div className="bg-white rounded-lg p-4 mb-1 border border-gray-100 shadow-sm">
-      <div className="flex justify-between items-center mb-2">
-        <h3 className="text-lg font-medium capitalize">{category}</h3>
-        <span className={`font-bold text-lg ${priceColor}`}>{total}</span>
+    <div className="bg-white rounded-lg p-3 border border-gray-100 shadow">
+      <div className="flex justify-between items-center mb-2 text-lg">
+        <h3 className="font-medium capitalize">{category}</h3>
+        <span className={`font-bold ${priceColor}`}>{total}</span>
       </div>
       <div className="relative h-2 bg-gray-100 rounded-full overflow-hidden">
         <div 
