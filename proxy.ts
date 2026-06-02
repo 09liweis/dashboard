e410c29e58f01aab05d1225410f7baf8221d2d9e
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2026-06-02 20:27:59 +0000

    use proxy instead of middleware

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,11 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  // retrieve the current response
+export function proxy(request: NextRequest) {
   const res = NextResponse.next()
 
-  // add the CORS headers to the response
-  res.headers.append('Access-Control-Allow-Credentials', "true")
-  res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.headers.append('Access-Control-Allow-Credentials', 'true')
+  res.headers.append('Access-Control-Allow-Origin', '*')
   res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
   res.headers.append(
     'Access-Control-Allow-Headers',
@@ -17,7 +15,6 @@ export function middleware(request: NextRequest) {
   return res
 }
 
-// specify the path regex to apply the middleware to
 export const config = {
   matcher: '/api/:path*',
 }
