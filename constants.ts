dc8fcf28c6197d68b983404f44dcbb4e02d1661f
Sam Li <weisen.li@hotmail.com>
2026-06-08 17:59:20 +0000

    remove unused api endpoint

--- constants.ts
+++ constants.ts
@@ -2,8 +2,4 @@ export const API_URL = 'https://samliweisen.onrender.com/api/';
 
 export const COMMENT_LIST_API = `${API_URL}comments`;
 
-export const BLOG_LIST_API = `${API_URL}blogs`;
-
-export const VIDEO_LIST_API = `${API_URL}videos/bilibili`;
-
 export const STATS_LIST_API = `${API_URL}stats`;
