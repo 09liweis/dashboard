0346f1b3719048835b62b177de35e38cec3ba407
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2026-06-10 14:20:56 +0000

    refactor NAV_LINKS

--- constants.ts
+++ constants.ts
@@ -3,3 +3,12 @@ export const API_URL = 'https://samliweisen.onrender.com/api/';
 export const COMMENT_LIST_API = `${API_URL}comments`;
 
 export const STATS_LIST_API = `${API_URL}stats`;
+
+export const NAV_LINKS = [
+  { tl: "Home", url: "/", icon: "house-user" },
+  { tl: "About", url: "/resume", icon: "user" },
+  { tl: "Cost Calculator", url: "/calculator", icon: "book" },
+  { tl: "Blogs", url: "/blogs", icon: "piggy-bank" },
+  { tl: "FAQs", url: "/faq", icon: "comments" },
+  { tl: "Comments", url: "/comments", icon: "comment" },
+] as const;
